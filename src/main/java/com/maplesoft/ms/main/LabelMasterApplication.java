bd3d0c238b71db5502faca13b3cc8772d273715e
gambareh <v.blagojevic1985@gmail.com>
2021-04-13 13:02:02 +0200

    init commit

---
+++ src/main/java/com/maplesoft/ms/main/LabelMasterApplication.java
@@ -0,0 +1,13 @@
+package com.maplesoft.ms.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabelMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabelMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maplesoft/ms/main/LabelMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maplesoft.ms.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabelMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
