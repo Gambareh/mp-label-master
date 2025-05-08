5a38e0c1b12ae71f9c8560f59c052c143949000e
Vladimir Blagojevic <v.blagojevic1985@gmail.com>
2025-05-08 15:24:28 +0200

    Addin data
    adding new data

--- src/main/java/com/maplesoft/ms/main/persistance/models/Etiquette.java
+++ src/main/java/com/maplesoft/ms/main/persistance/models/Etiquette.java
@@ -1,38 +1,5 @@
 package com.maplesoft.ms.main.persistance.models;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-
-import javax.persistence.*;
-
-
-@ToString
-@NoArgsConstructor
-@Getter
-@Setter
-@Entity
-@Table(name = "sm_etiquette")
-public abstract class BasicEtiquette {
-
-    @Id
-    private Long id;
-    @Column(name = "label_path")
-    private String labelUrl;
-    @Column
-    private String title;
-    @Column
-    private String header;
-    @Column
-    private String message1;
-    @Column
-    private String message2;
-    @Column
-    private String message3;
-    @Column
-    private String message4;
-    @Column
-    private String message5;
+public class Etiquette {
 
 }
